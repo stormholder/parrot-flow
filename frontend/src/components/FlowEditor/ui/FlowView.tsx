abb9ab4f67ec2e87b7ee5523f2ddc8a41fae7f22
stormholder <dantedmc78@gmail.com>
2025-10-03 20:56:57 +0300

    fixed FlowView component

--- frontend/src/components/FlowEditor/ui/FlowView.tsx
+++ frontend/src/components/FlowEditor/ui/FlowView.tsx
@@ -1,42 +1,47 @@
 import ReactFlow, { MiniMap, Background } from "reactflow";
 import "reactflow/dist/style.css";
-import type { RFState } from "../store/types";
 import { nodes as toolboxNodes } from "../model/nodes";
 import type { Edge, Node } from "@/api-client";
+import type { Node as RFNode } from "reactflow";
 import useStore from "../store";
 import { useState } from "react";
 import nodeTypes from "./nodes";
-
-const selector = (state: RFState) => ({
-  flowNodes: state.nodes,
-  flowEdges: state.edges,
-  appendNode: state.appendNode,
-  appendEdge: state.appendEdge,
-  clearFlow: state.clear,
-});
-
+import type { NodeTypes } from "@shared/types/nodes";
+import type { DraggableNode } from "../model";
 export interface FlowViewProps {
   nodes: Node[];
   edges: Edge[];
 }
 
-export const FlowView: React.FC<FlowViewProps> = ({ nodes, edges }) => {
-  const [isInitialRender, setIsInitialRender] = useState(true);
-  const { flowNodes, flowEdges, appendNode, appendEdge, clearFlow } =
-    useStore(selector);
+const getNodeColor = (n: RFNode<DraggableNode>): string => {
+  if (n.style?.backgroundColor) return n.style?.backgroundColor;
+  if (!!n.type && n.type in toolboxNodes)
+    return toolboxNodes[n.type as NodeTypes].color!;
+  return "#e2e2e2";
+};
+
+function FlowView(props: Readonly<FlowViewProps>) {
+  const [isInitialRender, setIsInitialRender] = useState<boolean>(true);
+  const {
+    nodes: flowNodes,
+    edges: flowEdges,
+    appendNode,
+    appendEdge,
+    clear: clearFlow,
+  } = useStore();
 
   if (isInitialRender) {
     setIsInitialRender(false);
     clearFlow();
-    nodes.forEach((n) => appendNode(n));
-    edges.forEach((e) => appendEdge(e));
+    props.nodes.forEach((n) => appendNode(n));
+    props.edges.forEach((e) => appendEdge(e));
   }
 
   return (
     <ReactFlow
+      nodeTypes={nodeTypes}
       nodes={flowNodes}
       edges={flowEdges}
-      nodeTypes={nodeTypes}
       fitView
       proOptions={{ hideAttribution: true }}
       maxZoom={2}
@@ -48,14 +53,11 @@ export const FlowView: React.FC<FlowViewProps> = ({ nodes, edges }) => {
     >
       <MiniMap
         className="bg-white dark:bg-neutral-900"
-        nodeColor={(n) => {
-          if (n.style?.backgroundColor) return n.style?.backgroundColor;
-          if (!!n.type && n.type in toolboxNodes)
-            return (toolboxNodes as any)[n.type].color;
-          return "#e2e2e2";
-        }}
+        nodeColor={getNodeColor}
       />
       <Background gap={10} className="bg-gray-100 dark:bg-neutral-900" />
     </ReactFlow>
   );
-};
+}
+
+export default FlowView;
