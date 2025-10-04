74968cd2e3546af567454aaf36687f3c20dc2e85
stormholder <dantedmc78@gmail.com>
2025-10-04 15:38:51 +0300

    added CostonDndContext and CustomDragOverlay for flow editor

---
+++ frontend/src/components/FlowEditor/ui/CustomDndContext.tsx
@@ -0,0 +1,50 @@
+import { useCallback, useId } from "react";
+import { useReactFlow } from "reactflow";
+import { DndContext, type DragEndEvent } from "@dnd-kit/core";
+import { nanoid } from "@/utils";
+import { getNodeConfig } from "../model/nodes";
+import CustomDragOverlay from "./CustomDragOverlay";
+
+interface CustomDndContextProps {
+  children: React.ReactNode;
+}
+
+const CustomDndContext = ({ children }: CustomDndContextProps) => {
+  const ctxId = useId();
+  const { setNodes, screenToFlowPosition } = useReactFlow();
+
+  const onDragEnd = useCallback(
+    ({ active }: DragEndEvent) => {
+      const type = active.data.current?.type;
+      const data = active.data.current?.data;
+      if (!type) return;
+
+      const el = document.getElementById("overlay-id")!;
+      const clientRect = el.getBoundingClientRect();
+
+      const position = screenToFlowPosition({
+        x: clientRect.left,
+        y: clientRect.top,
+      });
+      const ID = nanoid();
+      const newNode = {
+        id: ID,
+        type,
+        position,
+        data: getNodeConfig(type, ID, data),
+      };
+
+      setNodes((nds) => nds.concat(newNode));
+    },
+    [screenToFlowPosition, setNodes]
+  );
+
+  return (
+    <DndContext id={ctxId} onDragEnd={onDragEnd}>
+      <CustomDragOverlay />
+      {children}
+    </DndContext>
+  );
+};
+
+export default CustomDndContext;
