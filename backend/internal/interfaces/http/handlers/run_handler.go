9dd6b435f5f89018301e00f1331f351d759d02d1
stormholder <dantedmc78@gmail.com>
2025-10-05 20:16:02 +0300

    added route handlers

---
+++ backend/internal/interfaces/http/handlers/run_handler.go
@@ -0,0 +1,129 @@
+package handlers
+
+import (
+	"context"
+	commandRun "parrotflow/internal/application/command/run"
+	queryRun "parrotflow/internal/application/query/run"
+	"parrotflow/internal/domain/run"
+	"parrotflow/internal/domain/scenario"
+	"parrotflow/internal/interfaces/http/commands"
+	"parrotflow/internal/interfaces/http/mappers"
+	"parrotflow/internal/interfaces/http/queries"
+)
+
+type RunHandler struct {
+	createCommandHandler *commandRun.CreateRunCommandHandler
+	startCommandHandler  *commandRun.StartRunCommandHandler
+	getQueryHandler      *queryRun.GetRunQueryHandler
+	listQueryHandler     *queryRun.ListRunsQueryHandler
+}
+
+func NewRunHandler(
+	createCommandHandler *commandRun.CreateRunCommandHandler,
+	startCommandHandler *commandRun.StartRunCommandHandler,
+	getQueryHandler *queryRun.GetRunQueryHandler,
+	listQueryHandler *queryRun.ListRunsQueryHandler,
+) *RunHandler {
+	return &RunHandler{
+		createCommandHandler: createCommandHandler,
+		startCommandHandler:  startCommandHandler,
+		getQueryHandler:      getQueryHandler,
+		listQueryHandler:     listQueryHandler,
+	}
+}
+
+func (h *RunHandler) CreateRun(ctx context.Context, req *commands.CreateRunRequest) (*commands.CreateRunResponse, error) {
+	scenarioID, err := scenario.NewScenarioID(req.Body.ScenarioID)
+	if err != nil {
+		return nil, err
+	}
+
+	cmd := commandRun.CreateRunCommand{
+		ScenarioID: scenarioID,
+		Parameters: req.Body.Parameters,
+	}
+
+	run, err := h.createCommandHandler.Handle(ctx, cmd)
+	if err != nil {
+		return nil, err
+	}
+
+	response := mappers.ToCreateRunResponse(run)
+
+	return response, nil
+}
+
+func (h *RunHandler) GetRun(ctx context.Context, req *queries.GetRunRequest) (*queries.GetRunResponse, error) {
+	runID, err := run.NewRunID(req.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	query := queryRun.GetRunQuery{
+		ID: runID,
+	}
+
+	run, err := h.getQueryHandler.Handle(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+
+	response := mappers.ToGetRunResponse(run)
+
+	return response, nil
+}
+
+func (h *RunHandler) ListRuns(ctx context.Context, req *queries.ListRunsRequest) (*queries.ListRunsResponse, error) {
+	criteria := run.NewSearchCriteria()
+	if req.ScenarioID != "" {
+		scenarioID, err := scenario.NewScenarioID(req.ScenarioID)
+		if err != nil {
+			return nil, err
+		}
+		criteria = criteria.WithScenarioID(scenarioID)
+	}
+	if req.Status != "" {
+		criteria = criteria.WithStatus(req.Status)
+	}
+	if req.Page > 0 {
+		criteria = criteria.WithPagination(req.RPP, (req.Page-1)*req.RPP)
+	}
+
+	query := queryRun.ListRunsQuery{
+		Criteria: criteria,
+	}
+
+	runs, err := h.listQueryHandler.Handle(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+
+	response := mappers.ToListRunResponse(runs, req.Page, req.RPP)
+
+	return response, nil
+}
+
+func (h *RunHandler) StartRun(ctx context.Context, req *commands.StartRunRequest) (*commands.StartRunResponse, error) {
+	runID, err := run.NewRunID(req.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	cmd := commandRun.StartRunCommand{
+		RunID: runID,
+	}
+
+	run, err := h.startCommandHandler.Handle(ctx, cmd)
+	if err != nil {
+		return nil, err
+	}
+
+	response := &commands.StartRunResponse{}
+	response.Body.ID = run.Id.String()
+	response.Body.Status = run.Status.String()
+	if run.StartedAt != nil {
+		response.Body.StartedAt = run.StartedAt.Time().Format("2006-01-02T15:04:05Z")
+	}
+
+	return response, nil
+}
---
+++ backend/internal/interfaces/http/handlers/scenario_handler.go
@@ -0,0 +1,144 @@
+package handlers
+
+import (
+	"context"
+	command "parrotflow/internal/application/command/scenario"
+	query "parrotflow/internal/application/query/scenario"
+	"parrotflow/internal/domain/scenario"
+	"parrotflow/internal/interfaces/http/commands"
+	"parrotflow/internal/interfaces/http/mappers"
+	"parrotflow/internal/interfaces/http/queries"
+)
+
+type ScenarioHandler struct {
+	createCommandHandler *command.CreateScenarioCommandHandler
+	updateCommandHandler *command.UpdateScenarioCommandHandler
+	deleteCommandHandler *command.DeleteScenarioCommandHandler
+	getQueryHandler      *query.GetScenarioQueryHandler
+	listQueryHandler     *query.ListScenariosQueryHandler
+}
+
+func NewScenarioHandler(
+	createCommandHandler *command.CreateScenarioCommandHandler,
+	updateCommandHandler *command.UpdateScenarioCommandHandler,
+	deleteCommandHandler *command.DeleteScenarioCommandHandler,
+	getQueryHandler *query.GetScenarioQueryHandler,
+	listQueryHandler *query.ListScenariosQueryHandler,
+) *ScenarioHandler {
+	return &ScenarioHandler{
+		createCommandHandler: createCommandHandler,
+		updateCommandHandler: updateCommandHandler,
+		deleteCommandHandler: deleteCommandHandler,
+		getQueryHandler:      getQueryHandler,
+		listQueryHandler:     listQueryHandler,
+	}
+}
+
+func (h *ScenarioHandler) CreateScenario(ctx context.Context, req *commands.CreateScenarioRequest) (*commands.CreateScenarioResponse, error) {
+	cmd := command.CreateScenarioCommand{
+		Name:        req.Body.Name,
+		Description: req.Body.Description,
+		Tag:         req.Body.Tag,
+		Icon:        req.Body.Icon,
+	}
+
+	scenario, err := h.createCommandHandler.Handle(ctx, cmd)
+	if err != nil {
+		return nil, err
+	}
+
+	response := mappers.ToCreateScenarioResponse(scenario)
+
+	return response, nil
+}
+
+func (h *ScenarioHandler) GetScenario(ctx context.Context, req *queries.GetScenarioRequest) (*queries.GetScenarioResponse, error) {
+	scenarioID, err := scenario.NewScenarioID(req.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	query := query.GetScenarioQuery{
+		ID: scenarioID,
+	}
+
+	scenario, err := h.getQueryHandler.Handle(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+
+	response := mappers.ToGetScenarioResponse(scenario)
+
+	return response, nil
+}
+
+func (h *ScenarioHandler) ListScenarios(ctx context.Context, req *queries.ListScenariosRequest) (*queries.ListScenariosResponse, error) {
+	criteria := scenario.NewSearchCriteria()
+	if req.Name != "" {
+		criteria = criteria.WithName(req.Name)
+	}
+	if req.Tag != "" {
+		criteria = criteria.WithTag(req.Tag)
+	}
+	if req.Page > 0 {
+		criteria = criteria.WithPagination(req.RPP, (req.Page-1)*req.RPP)
+	}
+
+	query := query.ListScenariosQuery{
+		Criteria: criteria,
+	}
+
+	scenarios, err := h.listQueryHandler.Handle(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+
+	response := mappers.ToListScenarioResponse(scenarios, req.Page, req.RPP)
+
+	return response, nil
+}
+
+func (h *ScenarioHandler) UpdateScenario(ctx context.Context, req *commands.UpdateScenarioRequest) (*commands.UpdateScenarioResponse, error) {
+	scenarioID, err := scenario.NewScenarioID(req.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	cmd := command.UpdateScenarioCommand{
+		ID:          scenarioID,
+		Name:        req.Body.Name,
+		Description: req.Body.Description,
+		Tag:         req.Body.Tag,
+		Icon:        req.Body.Icon,
+	}
+
+	scenario, err := h.updateCommandHandler.Handle(ctx, cmd)
+	if err != nil {
+		return nil, err
+	}
+
+	response := mappers.ToScenarioUpdateResponse(scenario)
+
+	return response, nil
+}
+
+func (h *ScenarioHandler) DeleteScenario(ctx context.Context, req *commands.DeleteScenarioRequest) (*commands.DeleteScenarioResponse, error) {
+	scenarioID, err := scenario.NewScenarioID(req.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	cmd := command.DeleteScenarioCommand{
+		ID: scenarioID,
+	}
+
+	err = h.deleteCommandHandler.Handle(ctx, cmd)
+	if err != nil {
+		return nil, err
+	}
+
+	response := &commands.DeleteScenarioResponse{}
+	response.Body.Success = true
+
+	return response, nil
+}
