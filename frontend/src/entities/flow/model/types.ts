02b6ca1e82c4802c3a074305d0ef48c6fcf4d196
stormholder <dantedmc78@gmail.com>
2025-10-08 15:41:25 +0300

    refactor - added flow entity; refactor - added flow entity model

---
+++ frontend/src/entities/flow/model/types.ts
@@ -0,0 +1,15 @@
+// Re-export React Flow types
+export type {
+  Node,
+  Edge,
+  Connection,
+  NodeChange,
+  EdgeChange,
+  HandleType,
+} from "reactflow";
+
+export type {
+  RFState,
+  NewNodeProps,
+  NewEdgeProps,
+} from "@/widgets/flow-editor/store/types";
