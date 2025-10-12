0151440b3e13dd385cae39dd5ee6c9e4f0cf55e4
stormholder <dantedmc78@gmail.com>
2025-10-12 13:53:07 +0300

    added ports imports

---
+++ agent/src/ports/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Ports Index
+ *
+ * Export all port interfaces for easy importing.
+ */
+
+export type { IMessageConsumer } from './messaging/IMessageConsumer.js';
+export type { IMessagePublisher, PublishOptions } from './messaging/IMessagePublisher.js';
+export type { IHealthMonitor, AgentStatus } from './monitoring/IHealthMonitor.js';
